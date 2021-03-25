5a60a625756318aec46324b11d33ed8160fa270b
NathanTempleton <nathan.templeton@ocsbstudent.ca>
2021-03-25 14:43:01 +0000

    Fixed errors and added comments

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// These will be used later so I've set them to zero for now.
 let numGuess = 0
 let num = 0
-num = Math.floor(Math.random() *10) +1
+// This will create a random number that the player has to guess.
+num = Math.floor(Math.random() * 10) + 1
 num = parseInt(num)
 
-while (numGuess != num) {
+// This lopp will have it so it continues until the player guesses a number right in the promt.
+while (numGuess !== num) {
   numGuess = prompt('Guess a number between 1 and 10: ')
   numGuess = parseInt(numGuess)
   if (numGuess > num) {
